964ba5b9157b94c483fec5b785a36e566867a542
danielzuot <danielzuot@gmail.com>
2016-06-10 11:54:44 -0400

    following Anirudh; bug fix, fill_in needs to be static

--- allinone/ns-allinone-2.35/ns-2.35/pause/pause_header.h
+++ allinone/ns-allinone-2.35/ns-2.35/pause/pause_header.h
@@ -22,7 +22,7 @@
    hdr_pause();
  
    /* Fill in fields */
-   void fill_in(Packet* p,
+   static void fill_in(Packet* p,
                 const std::vector<uint16_t> & s_class_pause_durations,
                 const std::vector<bool> & s_class_enable_vector);
  };
