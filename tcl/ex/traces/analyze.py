925755b95d9986fd3445da785302a48eea9f1dcf
danielzuot <danielzuot@gmail.com>
2016-06-10 09:30:48 -0400

    analyzing queue drops, parameter changes @Mohammad

--- tcl/ex/traces/analyze.py
+++ tcl/ex/traces/analyze.py
@@ -5,23 +5,22 @@ import matplotlib.pyplot as plt
 trace_data = np.loadtxt('mytracefile.tr')
 thr_data = np.loadtxt('thrfile.tr')
 
-# plot window sizes of specific flow ids
+################### plot window sizes of specific flow ids ##################
 fig = plt.figure()
 fids = [0, 1, 2]
 window_col_offset = 1
 for fid in fids:
     plt.plot(trace_data[:,0], trace_data[:,fid+window_col_offset], label = 'fid '+str(fid))
-
-axes = plt.gca()
+# axes = plt.gca()
 # axes.set_xlim([0.4, 0.8])
-axes.set_ylim([0,100])
+# axes.set_ylim([0,100])
 plt.title('Progression of TCP window sizes')
 plt.xlabel('time (s)')
 plt.ylabel('window size (packets)')
 plt.legend()
 plt.show()  
 
-# plot queue length
+################### plot queue length ##################
 fig = plt.figure()
 queue_length_col = 7
 plt.plot(trace_data[:,0], trace_data[:,queue_length_col])
@@ -34,9 +33,25 @@ plt.ylabel('queue length (packets)')
 plt.legend()
 plt.show()  
 
-# plot instantaneous throughputs
+################### plot queue drops ##################
+fig = plt.figure()
+queue_drops_col = 8
+plt.plot(trace_data[:,0], trace_data[:,queue_drops_col])
+# axes = plt.gca()
+# axes.set_xlim([0.4, 0.8])
+# axes.set_ylim([0,10])
+plt.title('Queue drops at main link')
+plt.xlabel('time (s)')
+plt.ylabel('cumulative drops (packets)')
+plt.legend()
+plt.show()  
+
+################## plot instantaneous throughputs ##################
 fig = plt.figure()
 plt.plot(thr_data[:,0], thr_data[:,1], label='total') #plot total throughput
+# axes = plt.gca()
+# axes.set_xlim([0.4, 0.8])
+# axes.set_ylim([0,10])
 fids = [0, 1, 2]
 thr_col_offset = 2
 for fid in fids:
@@ -47,3 +62,5 @@ plt.xlabel('time (s)')
 plt.ylabel('throughput (Mbs)')
 plt.legend()
 plt.show()  
+
+
